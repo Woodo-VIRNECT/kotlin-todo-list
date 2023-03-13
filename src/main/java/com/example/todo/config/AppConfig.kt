19ec36d21404c404ff553b8b05dc68d02619f498
dnejdzlr2 <dnejdzlr2@virnect.com>
2023-03-13 18:15:38 +0900

    first commit

---
+++ src/main/java/com/example/todo/config/AppConfig.kt
@@ -0,0 +1,14 @@
+package com.example.todo.config
+
+/**
+ * Project        : todo
+ * DATE           : 2023/03/13
+ * AUTHOR         : dnejdzlr2 (Woodo Lee)
+ * EMAIL          : dnejdzlr2@virnect.com
+ * DESCRIPTION    :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/03/13      dnejdzlr2          최초 생성
+ */class AppConfig {
+}
---
+++ src/test/java/com/example/todo/repository/TodoRepositoryImplTest.kt
@@ -0,0 +1,23 @@
+package com.example.todo.repository
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+/**
+ * Project        : todo
+ * DATE           : 2023/03/13
+ * AUTHOR         : dnejdzlr2 (Woodo Lee)
+ * EMAIL          : dnejdzlr2@virnect.com
+ * DESCRIPTION    :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/03/13      dnejdzlr2          최초 생성
+ */
+internal class TodoRepositoryImplTest {
+
+    @Test
+    fun save() {
+    }
+}
